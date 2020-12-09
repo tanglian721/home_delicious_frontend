c813b32b64e4b14dde25af88af97015193ab91f5
Liang <taylor721@gmail.com>
2020-12-08 17:01:36 -0700

    finish the title component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <logo-title />
+    <search-bar />
+    <trend-food-list />
+    <category-bar />
+    <food-card-list />
+    <bottom-bar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import BottomBar from '../components/bottomBar.vue';
+import CategoryBar from '../components/categoryBar.vue';
+import FoodCardList from '../components/foodCardList.vue';
+import LogoTitle from '../components/logoTitle.vue';
+import SearchBar from '../components/searchBar.vue';
+import TrendFoodList from '../components/trendFoodList.vue';
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BottomBar,
+    LogoTitle,
+    SearchBar,
+    TrendFoodList,
+    CategoryBar,
+    FoodCardList,
   }
 };
+
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variable.scss";
+.home{
+  width: 100vw;
+  height: 100vh;
+  background-color:$bgc;
+  position: relative;
+  .logo-title{
+    width: 70%;
+    height: 10vh;
+    margin-left: 15%;
+  }
+  .search-bar{
+    width: 100%;
+    height: 8vh;
+    background-color: rgb(104, 71, 255);
+    position: sticky;
+    top: 0;
+  }
+  .trend-food-list{
+    width: 100%;
+    height: 40vh;
+    background-color: rgb(138, 58, 58);
+  }
+  .category-bar{
+    width: 100%;
+    height: 10vh;
+    background-color: #fff;
+    position: sticky;
+    top: 8vh;
+  }
+  .food-card-list{
+    width: 100%;
+    height: 200vh;
+    background-color: rgb(107, 138, 58);
+  }
+  .bottom-bar{
+    z-index: 50;
+    width: 100%;
+    height: 10vh;
+    background-color: #fff;
+    position: fixed;
+    bottom:0
+  }
+}
+</style>
