e0c0673f14855297d3c108c7dfe4734178a19983
Liang <taylor721@gmail.com>
2020-12-10 21:26:44 -0700

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Food from "../views/Food.vue";
+import uploadFood from "../views/uploadFood.vue"
 
 Vue.use(VueRouter);
 
@@ -13,6 +14,10 @@ const routes = [{
     path: "/food/*",
     name: "food",
     component: Food
+}, {
+    path: "/upload",
+    name: "upload",
+    component: uploadFood
 }, ];
 
 const router = new VueRouter({
